71f996bfe40e2c5ed266f1985a6a58d77862ddaf
abssourav <abubakkarsiddique1781@gmail.com>
2024-03-17 09:16:06 +0600

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline text-red-500">
+                Hello world!
+            </h1>
+            Home
+            <Header />
+        </div>
+    )
+}
+
+export default Home
